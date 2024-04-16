c2fb46a16e981b5ce041eae17371eb297bc48e31
wellington chida <wellingtonchida@protonmail.com>
2024-04-16 06:07:02 -0300

    add create and access acount

--- bank.py
+++ bank.py
@@ -1,30 +1,81 @@
 import operations
+users = {}
+accounts = {}
 
-print(
-    """
-    1 - sacar
-    2 - deposit
-    3 - extrato
-    4 - sair
-    """
-)
+def create_user():
+    cpf = input("Digite seu cpf: ")
+    if cpf in users:
+        print("Usuário já cadastrado")
+        return
+    user = {
+        'name': input("Digite seu nome: "),
+        'cpf': cpf,
+        'idade': input("Digite sua idade: ")
+    }
+    users[cpf] = user
+    create_account(cpf)
 
-choice = input("Digite a opção desejada: ")
+def create_account(cpf):
+    account = {
+        'user': users[cpf],
+        'agency': '0001',
+        'balance': 0,
+        'hist': f"valor:{0} operação: inicialização\n",
+        'withdrawn': 0
+    }
+    accounts[cpf] = account
 
-balance = 0
-hist = f"valor:{balance} operação: inicialização\n"
-withdrawn = 0
 
-while choice != '4':
-    if choice == '1':
-         balance, hist, withdrawn = operations.withdrawn(balance=balance, hist=hist, withdrawn=withdrawn)
-    elif choice == '2':
-        balance, hist = operations.deposit(balance= balance, hist= hist)
-    elif choice == '3':
-        operations.extract(hist= hist)
+
+def access_account():
+    
+    cpf = input("Digite seu cpf: ")
+    if cpf in users:
+        account = accounts[cpf]
+        print(
+            """
+            1 - sacar
+            2 - deposit
+            3 - extrato
+            4 - sair
+            """
+        )
+        choice = input("Digite a opção desejada: ")
+        while choice != '4':
+            if choice == '1':
+               account['balance'], account['hist'], account['withdrawn'] = operations.withdrawn(balance=account['balance'], hist=account['hist'], withdrawn=account['withdrawn'])
+            elif choice == '2':
+               account['balance'], account['hist'] = operations.deposit(balance= account['balance'], hist= account['hist'])
+            elif choice == '3':
+                operations.extract(hist= account['hist'])
+            else:
+                print("Opção inválida")
+            choice = input("Digite a opção desejada: ")
+        print("Obrigado por usar nosso banco")
+    else:
+        print("Usuário não encontrado")
+        
+
+
+choice_actions = {
+    '0': exit,
+    '1': create_user,
+    '2': access_account
+}
+
+while True:
+    print("""
+    Bem vindo ao banco
+    Digite a opção desejada
+    0 - sair
+    1 - criar conta
+    2 - acessar conta
+    """)
+
+    choice = input("Digite a opção desejada: ")
+    action = choice_actions.get(choice)
+
+    if action:
+        action()
     else:
         print("Opção inválida")
-    
-    choice = input("Digite a opção desejada: ")
-    
-print("Obrigado por usar nosso banco")
--- operations.py
+++ operations.py
@@ -1,16 +1,16 @@
 def withdrawn(balance: float, hist: str, withdrawn: float):
-    value= float(input("Digite o value a ser sacado: "))
+    value= float(input("Digite o valor a ser sacado: "))
     if balance < value or value > 500 or withdrawn + value > 500:
-        print("balance insuficiente, value acima do permitido ou limite diário atingido")
-        hist += f"value:{value} operação: saque negado\n"
+        print("Saldo insuficiente, valor acima do permitido ou limite diário atingido")
+        hist += f"valor:{value} operação: saque negado\n"
     else:
         balance -= value
         withdrawn += value
-        hist += f"value:{value} operação: saque\n"
+        hist += f"valor:{value} operação: saque\n"
     return balance, hist, withdrawn
 
 def deposit(balance: float, hist: str):
-    value = float(input("Digite o value a ser depositado: "))
+    value = float(input("Digite o valor a ser depositado: "))
     balance += value 
     hist += f"value:{value} operação: deposito\n"
     return balance, hist
